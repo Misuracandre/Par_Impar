d7d8e8de59e8eba16dcb548fe0c8b85302e703f5
André misuraca <m1sur4k@gmail.com>
2023-03-05 21:35:57 -0300

    Adicionar arquivos de projeto.

---
+++ numero par ou impar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lê o número do usuário
+        Console.WriteLine("Digite um número inteiro:");
+        int num = int.Parse(Console.ReadLine());
+
+        // Verifica se o número é par ou ímpar
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("O número é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar.");
+        }
+    }
+}
